23b2327e3087cdeb639c1f45d04ad8c238d3a722
AhmedAliMSoliman <ahmed-3010@hotmail.com>
2021-11-05 17:05:13 +0400

    staff just works!

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
 var express = require("express");
 var app = express();
+var port = process.env.PORT || 3000;
 
-var express = require("body-parser");
 
 var indexRoutes = require("./routes/index");
 var galaryRoutes = require("./routes/galary");
 
+
 app.set("view engine", "ejs");
+
+app.use(express.static(__dirname + '/public'));
 
 app.use(indexRoutes);
 app.use(galaryRoutes);
 
-app.listen(process.env.PORT || 3000, process.env.IP, function() {
-    console.log("Personal server started ...");
+app.listen(port, process.env.IP, function() {
+    console.log(`Personal server started  on port ${port}`);
 });
