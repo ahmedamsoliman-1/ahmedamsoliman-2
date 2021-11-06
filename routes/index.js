a4b3591a83e74d87252988500eac47da645c3994
AhmedAliMSoliman <ahmed-3010@hotmail.com>
2021-11-06 19:44:00 +0400

    update

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,27 @@
 var express = require("express");
 var router = express.Router();
 
-router.get("/", function(req, res) {
+router.get("/", function(req, res) 
+{
     res.render("landing")
 });
 
 
+router.get("/index", function(req, res) 
+{
+    res.render("index")
+});
 
 
-router.get("/register", function(req, res) {
+
+
+router.get("/register", function(req, res) 
+{
     res.render("register");
 });
 
-router.get("/login", function(req, res) {
+router.get("/login", function(req, res) 
+{
     res.render("login");
 });
 
