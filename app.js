9b04a8c81ac15b0aa1d3588bd1bf90b7a5b7b520
AhmedAliMSoliman <ahmed-3010@hotmail.com>
2021-11-11 00:44:42 +0400

    template finally works

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var port = process.env.PORT || 3000;
 var indexRoutes = require("./routes/index");
 var galaryRoutes = require("./routes/galary");
 var linksRoutes = require("./routes/links");
+// var anni = require("./scripts/main");
 
 
 app.set("view engine", "ejs");
@@ -15,6 +16,7 @@ app.use(express.static(__dirname + '/public'));
 app.use(indexRoutes);
 app.use(galaryRoutes);
 app.use(linksRoutes);
+// app.use(anni);
 
 app.listen(port, process.env.IP, function() {
     console.log(`Personal server started  on port ${port}`);
