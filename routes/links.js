aa41549f4925863828c13f88885a1d0052e596e6
Ahmed Ali M. Soliman <ahmed-3010@hotmail.com>
2021-11-14 23:20:09 +0400

    links athentication

--- routes/links.js
+++ routes/links.js
@@ -1,8 +1,12 @@
 var express = require("express");
 var router = express.Router();
 
-router.get("/links", function(req, res) {
-    res.render("links")
+var middleware = require("../middleware");
+
+
+router.get("/links", middleware.isLoggedIn , function(req, res) 
+{
+    res.render("links");
 });
 
 module.exports = router;
