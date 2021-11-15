6ccbf41c492834457bb10c1d5e4839168021f9e1
Ahmed Ali M. Soliman <ahmed-3010@hotmail.com>
2021-11-16 02:05:01 +0400

    revoke authentication from external links

--- routes/links.js
+++ routes/links.js
@@ -1,10 +1,9 @@
 var express = require("express");
 var router = express.Router();
 
-var middleware = require("../middleware");
 
 
-router.get("/links", middleware.isLoggedIn , function(req, res) 
+router.get("/links", function(req, res) 
 {
     res.render("links");
 });
