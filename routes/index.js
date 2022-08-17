8d2e920e0fa5322e0d1215c502b6d5b811f42c10
Ahmed Ali M. Soliman <40831708+ahmedamsoliman-1@users.noreply.github.com>
2022-08-17 11:53:19 +0400

    Remove ip

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,11 @@ var router = express.Router();
 var passport = require("passport");
 var User = require("../models/user");
 var middleware = require("../middleware");
-ipaddress = require("ip").address();
 
 
 router.get("/", function(req, res) 
 {
-    res.render("landing", {temp: ipaddress});
+    res.render("landing");
 });
 
 
