b70e45ad8f9d3c9b502c29d5e8076784b5200dc4
Ahmed Ali M. Soliman <40831708+ahmedamsoliman-1@users.noreply.github.com>
2023-02-17 00:30:12 +0400

    D

--- routes/index.js
+++ routes/index.js
@@ -4,68 +4,55 @@ var passport = require("passport");
 var User = require("../models/user");
 var middleware = require("../middleware");
 
-
-router.get("/", function(req, res) 
-{
-    res.render("landing");
+router.get("/", function (req, res) {
+  res.render("landing");
 });
 
-
-router.get("/index", function(req, res) 
-{
-    res.render("index");
+router.get("/index", function (req, res) {
+  // res.render("index");
+  res.redirect("http://webresume.s3-website-us-east-1.amazonaws.com/");
 });
-
-
 
 // Show register form
-router.get("/register", function(req, res)
-{
-    res.render("register");
+router.get("/register", function (req, res) {
+  res.render("register");
 });
 
 // Signup logic
-router.post("/register", function(req, res)
-{
-    var newUser = new User({username: req.body.username});
-    User.register(newUser, req.body.password, function(err, user)
-    {
-        if(err)
-        {
-            req.flash("error", err.message);
-            return res.render("register");
-        }
-        passport.authenticate("local")(req, res, function()
-        {
-            req.flash("success", "Welcome to Ahmed's site " + user.username);
-            res.redirect("/gallary");
-        });
+router.post("/register", function (req, res) {
+  var newUser = new User({ username: req.body.username });
+  User.register(newUser, req.body.password, function (err, user) {
+    if (err) {
+      req.flash("error", err.message);
+      return res.render("register");
+    }
+    passport.authenticate("local")(req, res, function () {
+      req.flash("success", "Welcome to Ahmed's site " + user.username);
+      res.redirect("/gallary");
     });
+  });
 });
 
-
 // Show login form
-router.get("/login", function(req, res)
-{
-    res.render("login");
+router.get("/login", function (req, res) {
+  res.render("login");
 });
 
 // Login logic
-router.post("/login", passport.authenticate("local", 
-{
+router.post(
+  "/login",
+  passport.authenticate("local", {
     successRedirect: "/gallary",
     failureRedirect: "/login",
-}), function(req, res){});
+  }),
+  function (req, res) {}
+);
 
 // Log out route
-router.get("/logout", function(req, res)
-{
-    req.logout();
-    req.flash("success", "Logged you out!")
-    res.redirect("/");
+router.get("/logout", function (req, res) {
+  req.logout();
+  req.flash("success", "Logged you out!");
+  res.redirect("/");
 });
-
-
-
 
 module.exports = router;
