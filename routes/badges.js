6fe60ade1b0e8d81f22d6a1bfa9cb22e6f914d02
Ahmed Ali M. Soliman <40831708+ahmedamsoliman-1@users.noreply.github.com>
2023-04-03 00:47:32 +0400

    update url

--- routes/badges.js
+++ routes/badges.js
@@ -4,6 +4,7 @@ var router = express.Router();
 
 let options = { json: true };
 
+// function: https://us-east-1.console.aws.amazon.com/lambda/home?region=us-east-1#/functions/return_urls_certs_and_badges_2?tab=code
 let new_url_2d =
   "https://bjessle3ffrosciqy5jklv46yu0bjqar.lambda-url.us-east-1.on.aws/";
 
