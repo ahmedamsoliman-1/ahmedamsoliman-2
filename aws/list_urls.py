b077e492b177df43eac67364a744fb669ed3c5d7
Ahmed Ali M. Soliman <40831708+ahmedamsoliman-1@users.noreply.github.com>
2023-05-30 18:17:18 +0400

    Update on Tue, May 30, 2023  6:17:18 PM

--- aws/list_urls.py
+++ aws/list_urls.py
@@ -1,4 +1,6 @@
 import boto3
+import json
+
 profile="aams-1-us-east-1"
 
 # Create a Boto3 client for AWS Lambda
@@ -11,33 +13,42 @@ response = lambda_client.list_functions()
 
 # Retrieve and print the function URL for each Lambda function
 for function in response['Functions']:
+
     function_name = function['FunctionName']
+    function_description = function['Description']
     
     # Retrieve the function URL configuration
     function_url_configs = lambda_client.list_function_url_configs(FunctionName=function_name)
     
-    # Extract the function URL from the configuration
-    function_url = function_url_configs['FunctionUrlConfigs'][0]['FunctionUrl']
+
+    # function_description = function_url_configs['FunctionUrlConfigs'][0]['Description']
+
+    if function_description == "Lambda function returns URLs from S3 Bucket":
+
+        print(f"Function Name: {function_name}")
+        # Extract the function URL from the configuration
+        function_url = function_url_configs['FunctionUrlConfigs'][0]['FunctionUrl']
     
-    print(f"Function Name: {function_name}")
-    print(f"Function URL: {function_url}")
-    print()
+    
+    
+        print(f"Function URL: {function_url}")
 
 
-    # Invoke the Lambda function
-    invoke_response = lambda_client.invoke(
-        FunctionName=function_name,
-        InvocationType='RequestResponse'
-    )
-    
-    # Extract the response payload
-    payload = invoke_response['Payload'].read().decode('utf-8')
-    
-    # Write the response JSON to a file
-    filename = f"{function_name}_response.json"
-    with open(filename, 'w') as file:
-        file.write(payload)
-    
-    print(f"Invoked function: {function_name}")
-    print(f"Response written to file: {filename}")
-    print()
+        # Invoke the Lambda function
+        invoke_response = lambda_client.invoke(
+            FunctionName=function_name,
+            InvocationType='RequestResponse'
+        )
+        
+        # Extract the response payload
+        payload = invoke_response['Payload'].read().decode('utf-8')
+        
+        # Write the response JSON to a file
+        filename = f"../public/data/{function_name}_response.json"
+        with open(filename, 'w') as file:
+            json.dump(json.loads(payload), file, indent=4)
+            # file.write(payload)
+        
+        print(f"Invoked function: {function_name}")
+        print(f"Response written to file: {filename}")
+        print()
